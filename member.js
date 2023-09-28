9e68395ef9a6d3280c7f656305b6234744c5bd1e
danielcheehong <danielcheehong@hotmail.com>
2023-09-28 01:41:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = document.getElementById('skills');
+    var member = document.getElementById('member');
+    var skillsMember = document.getElementById('skillsMember');
+    var memberSkills = document.getElementById('memberSkills');
+
+    skills.style.display = 'block';
+    member.style.display = 'none';
+    skillsMember.style.display = 'block';
+    memberSkills.style.display = 'none';
+}
